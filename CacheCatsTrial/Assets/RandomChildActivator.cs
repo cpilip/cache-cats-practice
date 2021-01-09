28c29b361e23086b5313c9fe3289b02732822f58
Jacob Dyer <dyerj@rpi.edu>
2021-01-09 05:10:12 -0500

    Walkaround, Collisions, and General Maps
    Title is self-explanatory.  Haven't yet figured out the level distributor.
    
    Co-Authored-By: cpilip <pilipchristina@gmail.com>

---
+++ CacheCatsTrial/Assets/RandomChildActivator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomChildActivator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random random = new Random();
+        int randomlyChosenIndex = random.Next(0, transform.childCount);
+
+        transform.GetChild(randomlyChosenIndex).setActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
