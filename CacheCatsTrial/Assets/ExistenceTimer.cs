7b3b844965af777ff57bc850f44ab21992242f7f
Jacob Dyer <dyerj@rpi.edu>
2021-01-10 18:35:04 -0500

    Move To Hub + Transparent Spawner
    Self explanatory.

--- CacheCatsTrial/Assets/ExistenceTimer.cs
+++ CacheCatsTrial/Assets/ExistenceTimer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExistenceTimer : MonoBehaviour
 {
@@ -23,5 +24,8 @@ public class ExistenceTimer : MonoBehaviour
     {
         // Do whatever.
         Debug.Log("End of " + time_needed + " second(s) timer!");
+
+        // In our case, it's "Go to the hub!"
+        SceneManager.LoadScene("hub");
     }
 }
